6d689d483c442a611c32131e156d063ecdf8b036
Victor Diniz <victorhugo10diniz@gmail.com>
2025-09-18 14:53:17 -0300

    Corrigindo imagens novamente

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,14 +6,6 @@ const nextConfig = {
     unoptimized: true,
     domains: ['localhost', 'res.cloudinary.com'],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://presence-imobiliaria-api.onrender.com/api/:path*',
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
